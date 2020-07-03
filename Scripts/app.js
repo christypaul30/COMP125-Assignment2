c41575ba6981ec3580df733db9bdc61b97a9c66a
Christy Paul <christypaul59@gmail.com>
2020-07-03 14:38:18 -0400

    changes in project and js file

--- Scripts/app.js
+++ Scripts/app.js
@@ -6,9 +6,24 @@
     function Start()
         {
             console.log('%cApp started...', "color:blue; font-size: 24px");           
-            
-            
+           
+            let miniPortfolio = document.getElementsByTagName("h1")[0].innerHTML;
 
+            console.log(miniPortfolio);
+
+            let aboutMe = document.getElementsByTagName("h1")[1].innerHTML;
+
+            console.log(aboutMe);
+
+            let paragraph = document.getElementsByClassName("paragraph")[0].textContent;
+
+            console.log(paragraph);
+
+            let mobApp = document.getElementsByName("container3")[0].textContent;
+
+            console.log(mobApp);
+
+            
         }    
     
 
