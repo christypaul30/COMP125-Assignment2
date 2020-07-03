f62fe4511c631e82d455673e65786c2d54241c2d
Christy Paul <christypaul59@gmail.com>
2020-07-02 23:23:07 -0400

    added paragraph stuff

--- Scripts/app.js
+++ Scripts/app.js
@@ -7,36 +7,6 @@
         {
             console.log('%cApp started...', "color:blue; font-size: 24px");           
             
-            let title = document.title;
-
-            title = title.toLowerCase();
-
-
-            console.log(`The title of the page is ${title}`);
-
-            let navAnchors = document.querySelectorAll("li a");
-
-            for (const anchor of navAnchors) 
-            {
-                let anchorString = anchor.getAttribute("href");
-                anchorString = anchorString.substr(0,anchorString.length - 5);
-
-                if ((title === "home") && (anchorString === "index") || (title === anchorString))
-                {
-                    anchor.className = "nav-link active";
-                }             
-            
-            }
-            //Step 1: hook into spot (element) on page
-            let jumbotron = document.getElementsByClassName("jumbotron")[0];
-            //Step 2: create new element
-            let newParagraph = document.createElement("p");
-            //step 3: configure new elemtn
-            newParagraph.textContent = title;
-            
-            //Step 4: attach new element
-            jumbotron.appendChild();
-
             
 
         }    
