ef929937d1c3989db3d6adc8a55c60bf5ad137c5
Christy Paul <christypaul59@gmail.com>
2020-07-03 16:21:34 -0400

    final app.js changes

--- Scripts/app.js
+++ Scripts/app.js
@@ -5,13 +5,38 @@
     //named function
     function Start()
         {
-            console.log('%cApp started...', "color:blue; font-size: 24px");           
+            console.log('%cApp started...', "color:blue; font-size: 24px");
+            
+            //Nav Link Highlighting
+            let title = document.title;
+
+            title = title.toLowerCase();
+
+            console.log(`The title of the page is ${title}`);
+
+            let navAnchors = document.querySelectorAll("li a");
+
+        for (const anchor of navAnchors) 
+        {
+
+            let anchorString = anchor.getAttribute("href");
+            anchorString = anchorString.substr(0,anchorString.length-5);
+
+            if((title === "home") && (anchorString === "index") || (title === anchorString))
+                {
+                    anchor.className = "nav-link active";
+                }
+
+        }
+
            
-            let miniPortfolio = document.getElementsByTagName("h1")[0].innerHTML;
+            /* // Question One: String Variables
+
+            let miniPortfolio = document.getElementsByTagName("h1")[0].textContent;
 
             console.log(miniPortfolio);
 
-            let aboutMe = document.getElementsByTagName("h1")[1].innerHTML;
+            let aboutMe = document.getElementsByTagName("h1")[1].textContent;
 
             console.log(aboutMe);
 
@@ -21,10 +46,52 @@
 
             let mobApp = document.getElementsByName("container3")[0].textContent;
 
-            console.log(mobApp);
+            console.log(mobApp); */
+            
+
+            //Question Three: String Variables
+
+            /* let cardText = document.getElementById("p")[0];
+            console.log (cardText);
+
+            let cardtext2 = document.getElementById("p")[1];
+            console.log(cardtext2);
+
+            let cardtext3 = document.getElementById("p")[2];
+            console.log(cardtext3); */
 
             
-        }    
+        }
+        
+        function addParagraphsToJumbotron() 
+    {
+        // step 1 hook into the spot (element) on the page
+        let paragraph = document.getElementsByClassName("paragraph")[0];
+
+        if (paragraph) 
+        {
+            // step 2 create a new element
+            let newParagraph = document.createElement("p");
+
+            // step 3 configure the new element
+            newParagraph.textContent =
+                `
+                This is an example paragraph.
+                Here is the next line.
+                
+                `;
+
+            // step 4 attach the new element
+            paragraph.appendChild(newParagraph);
+
+            return true;
+
+        }
+
+        return false;
+    }
+
+
     
 
     window.addEventListener("load", Start);
